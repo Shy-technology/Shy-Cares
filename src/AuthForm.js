589ff86784ef03aeceeed3e5c7642f294bdb99b2
Kush Gupta <kushgupta410@gmail.com>
2025-04-03 02:32:50 +0530

    Authform.js plug

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from "react";
 import { auth } from "./firebase";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
