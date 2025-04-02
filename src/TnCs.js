9e540b2fca6e1e669b991e3b546efebe7598a466
Kush Gupta <kushgupta410@gmail.com>
2025-04-03 01:43:37 +0530

    Few more changes

--- src/TnCs.js
+++ src/TnCs.js
@@ -1,4 +1,4 @@
-export default function TermsPage() {
+export default function TnCs() {
     return (
       <div className="max-w-3xl mx-auto p-4 text-gray-800">
         <h1 className="text-2xl font-bold mb-4">Terms & Conditions</h1>
